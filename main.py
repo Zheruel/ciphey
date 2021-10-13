2a724ed16fea46352e78f7d246e4bf1765cf29fc
Tin Zeljar <tinzeljar@gmail.com>
2021-10-13 21:58:04 +0200

    1.2.5
    - Testing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from db_models.shared import db
 from controllers.license_controller import license_controller
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/ciphey"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.register_blueprint(license_controller)
 
@@ -13,4 +13,4 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
